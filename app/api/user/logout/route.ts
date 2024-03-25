ef713542e6099bbe104188743c3014b921011823
nithinK-142 <nithin.sagar.359@gmail.com>
2024-03-25 10:05:03 +0530

    fix: logout API error

--- app/api/user/logout/route.ts
+++ app/api/user/logout/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const response = NextResponse.json({
-      message: "Logout Sucessful",
+      message: "Logout Successful",
       success: true,
     });
     response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
     return response;
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }), { status: 500 };
+    return NextResponse.json({ error: error.message }, { status: 500 });
   } finally {
     setTimeout(async () => {
       await Disconnect();
