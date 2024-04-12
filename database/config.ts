8c535268881e277bfc33eb11a94082689430903c
nithinK-142 <nithin.sagar.359@gmail.com>
2024-04-12 11:13:46 +0530

    refactoe: use existing dbConnection.

--- database/config.ts
+++ database/config.ts
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
 
-export const Connect = async () => {
+export const Connect = async (): Promise<void> => {
   try {
     const mongodbUri = process.env.MONGODB_URI;
-
     if (!mongodbUri) throw new Error("MONGODB_URI is not defined");
+
+    // Check if a connection already exists
+    if (mongoose.connection.readyState === 1) {
+      console.log("MongoDB connection already exists");
+      return;
+    }
 
     console.log("Connecting to MongoDB...");
     await mongoose.connect(mongodbUri);
     console.log("Connected to MongoDB.");
   } catch (error: any) {
     console.log("Error while connecting with the database", error.message);
+    process.exit(1);
   }
 };
 
-export const Disconnect = async () => {
+export const Disconnect = async (): Promise<void> => {
   try {
     console.log("Disconnecting from MongoDB...");
     await mongoose.disconnect();
